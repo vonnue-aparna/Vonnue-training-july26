043cafbe21141c60b7053814672dd6e9381ddd5b
anjal-vonnue <anjal.biju@vonnue.com>
2026-09-05 15:06:59 +0530

    feat: implement track page

--- src/pages/renderTrack.ts
+++ src/pages/renderTrack.ts
@@ -2,7 +2,36 @@ import { StoreType } from "../types/timeTypes";
 
 export function renderTrack(store: StoreType): HTMLElement {
   const section = document.createElement("section");
-  section.innerHTML = "<div>Track</div>";
+  section.innerHTML = `<div class="track-container">
+        <div class="add-task">
+          <div>TIMER:</div>
+          <div class="btn-container">
+            <button>Start Task</button>
+            <button>Pause Task</button>
+            <button>End Task</button>
+          </div>
+        </div>
+        <div class="log-container">
+          <article class="task">
+            <h3>Task Name</h3>
+            <p>StartTime:</p>
+            <p>EndTime:</p>
+            <p>TotalDuration:</p>
+          </article>
+          <article class="task">
+            <h3>Task Name</h3>
+            <p>StartTime:</p>
+            <p>EndTime:</p>
+            <p>TotalDuration:</p>
+          </article>
+          <article class="task">
+            <h3>Task Name</h3>
+            <p>StartTime:</p>
+            <p>EndTime:</p>
+            <p>TotalDuration:</p>
+          </article>
+        </div>
+      </div>`;
 
   return section;
 }
