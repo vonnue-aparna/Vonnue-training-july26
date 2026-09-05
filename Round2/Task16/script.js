264cf14784d40190b6e8218ab19100d8c58f6ba7
Gauresh P A <gauresh.pa@vonnue.com>
2026-09-05 17:18:32 +0530

    feat: deleted Round 2 and Round 3

--- Round2/Task16/script.js
+++
@@ -1,9 +0,0 @@
-function loadAppointments() {
-    const data = JSON.parse(localStorage.getItem("appointments"));
-
-    if (!data || data==="{bad}") {
-        return [];
-    }
-    return data;
-
-}
