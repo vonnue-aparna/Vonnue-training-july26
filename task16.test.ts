feeedaa114d76ca562cc2f486e88c9344cf1294d
anjal-vonnue <anjal.biju@vonnue.com>
2026-09-05 12:28:27 +0530

    feat: commented the implementation logic

--- task16.test.ts
+++ task16.test.ts
@@ -1,3 +1,6 @@
+// i think this can be tested by spying on the global Storage and the getItem method by mocking the implementation.
+// if that is the correct implementation i currently can't recall the syntax for it
+
 import { describe, test } from "vitest";
 
 describe("testing task 16", () => {
