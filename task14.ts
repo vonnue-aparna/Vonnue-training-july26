28b62d7b1c7abfa2677693bd25d0e7aeba800736
anjal-vonnue <anjal.biju@vonnue.com>
2026-09-05 11:18:47 +0530

    fix: added types, checks and sequential funtion calling

---
+++ task14.ts
@@ -0,0 +1,20 @@
+// fix
+// added types to appointments
+// changed the forEach loop to for loop so that the refreshCalender only runs after saving all appointmets
+// if appointments doest exist the saveAll funtion immediatley calls the refreshCalender Funtion
+
+type Appointment = {
+  name: string;
+  time: string;
+  age: number;
+};
+
+async function saveAll(appointments: Appointment[]) {
+  if (appointments) {
+    for (let i = 0; i < appointments.length; i++) {
+      let appointment = appointments[i];
+      await saveAppointment(appointment);
+    }
+  }
+  await refreshCalendar();
+}
